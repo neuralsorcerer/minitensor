429325b80fac70ce94b00b44a7182a364b43785e
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2025-09-04 11:41:18 +0530

    fix lints

--- bindings/src/error.rs
+++ bindings/src/error.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 Soumyadip Sarkar.
 // All rights reserved.
 //
-// This source code is licensed under the BSD-style license found in the
+// This source code is licensed under the Apache-style license found in the
 // LICENSE file in the root directory of this source tree.
 
 use engine::MinitensorError;
