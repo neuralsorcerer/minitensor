9fb78dd8f1a8c06dcb3d2b827e2e2701b8b9e58b
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2026-02-05 16:26:15 +0530

    meow

--- bindings/src/tensor.rs
+++ bindings/src/tensor.rs
@@ -5,20 +5,20 @@
 // LICENSE file in the root directory of this source tree.
 
 include!("tensor/preamble.rs");
-include!("tensor/pytensor_properties.rs");
-include!("tensor/pytensor_operations.rs");
-include!("tensor/pytensor_grad.rs");
-include!("tensor/pytensor_arithmetic.rs");
-include!("tensor/pytensor_comparison_dunder.rs");
-include!("tensor/pytensor_comparison.rs");
-include!("tensor/pytensor_reduction.rs");
-include!("tensor/pytensor_math.rs");
-include!("tensor/pytensor_numpy.rs");
-include!("tensor/pytensor_repr.rs");
-include!("tensor/pytensor_creation_basic.rs");
-include!("tensor/pytensor_creation_like.rs");
-include!("tensor/pytensor_creation_range.rs");
-include!("tensor/pytensor_concat_split.rs");
-include!("tensor/python_args.rs");
-include!("tensor/python_convert.rs");
-include!("tensor/python_numpy.rs");
+include!("tensor/pytensor/properties.rs");
+include!("tensor/pytensor/operations.rs");
+include!("tensor/pytensor/grad.rs");
+include!("tensor/pytensor/arithmetic.rs");
+include!("tensor/pytensor/comparison_dunder.rs");
+include!("tensor/pytensor/comparison.rs");
+include!("tensor/pytensor/reduction.rs");
+include!("tensor/pytensor/math.rs");
+include!("tensor/pytensor/numpy.rs");
+include!("tensor/pytensor/repr.rs");
+include!("tensor/pytensor/creation/basic.rs");
+include!("tensor/pytensor/creation/like.rs");
+include!("tensor/pytensor/creation/range.rs");
+include!("tensor/pytensor/concat_split.rs");
+include!("tensor/python/args.rs");
+include!("tensor/python/convert.rs");
+include!("tensor/python/numpy.rs");
