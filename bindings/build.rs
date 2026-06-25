41e0edba97b677f2037ca70f8760cac0ae0a8b6f
Soumyadip Sarkar <soumya.papanvk18@gmail.com>
2026-06-25 17:22:02 +0530

    Update build

--- bindings/build.rs
+++ bindings/build.rs
@@ -5,12 +5,9 @@
 // LICENSE file in the root directory of this source tree.
 
 fn main() {
+    // Add platform-specific linker arguments only when building the Python
+    // extension module. Regular Rust builds/tests must link libpython normally,
+    // while extension wheels must not link libpython for manylinux compliance.
+    #[cfg(feature = "extension-module")]
     pyo3_build_config::add_extension_module_link_args();
-    let config = pyo3_build_config::get();
-    if let Some(dir) = config.lib_dir() {
-        println!("cargo:rustc-link-search=native={}", dir);
-    }
-    if let Some(lib) = config.lib_name() {
-        println!("cargo:rustc-link-lib={}", lib);
-    }
 }
